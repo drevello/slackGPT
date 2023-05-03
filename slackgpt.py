e8ea2d0dbf926629107356fcd49ca251f94906a9
Danilo Orrego <dorrego87@bitbucket.org>
2023-05-02 23:55:41 -0300

    Created files

---
+++ slackgpt.py
@@ -0,0 +1,50 @@
+import os
+import openai
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+# Initialize the Slack app
+app = App()
+
+def chat_with_gpt(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+    )
+
+    message = response.choices[0].message["content"].strip()
+    return message
+
+app = App()
+
+@app.event("app_mention")
+async def handle_app_mentions(body, say):
+    print("App mention event received:", body)
+    text = body['event']['text']
+    user = body['event']['user']
+
+    openai.api_key = OPENAI_API_KEY
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": text}
+        ]
+    )
+    assistant_reply = response.choices[0].message.content
+
+    await say(f"<@{user}> {assistant_reply}")
+
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+    handler.start()
